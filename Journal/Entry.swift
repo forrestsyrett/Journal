0cacd8443040adff9803ae569f59328f7dac5f4e
forrestsyrett <forrest.syrett@me.com>
2016-05-01 14:19:58 -0600

    add

--- Journal/Entry.swift
+++ Journal/Entry.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+
+class Entry: Equatable {
+    
+    var timeStamp: NSDate
+    var title: String
+    var body: String
+    
+    
+    init(timeStamp: NSDate = NSDate(), title: String, body: String) {
+        
+        self.timeStamp = timeStamp
+        self.title = title
+        self.body = body
+    }
+}
+     func == (lhs: Entry, rhs: Entry) -> Bool {
+    return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+
--- Journal/EntryController.swift
+++ Journal/EntryController.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+
+class EntryController {
+    
+    static let sharedEntryController = EntryController()
+    
+    
+    var entries: [Entry]
+    
+    init() {
+        self.entries = []
+    }
+    
+    
+    func addEntry(entry: Entry) {
+        entries.append(entry)
+    }
+    
+    func removeEntry(entry: Entry) {
+        if let index = entries.indexOf(entry) {
+            entries.removeAtIndex(index)
+        }
+        
+    }
+}
+
+/*     func createPlaylist(name: String) {
+let playlist = Playlist(name: name)
+playlists.append(playlist)
+ */
