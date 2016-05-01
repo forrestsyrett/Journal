dfc0951bfb82a85c35d35a2a788403e58f5709b8
forrestsyrett <forrest.syrett@me.com>
2016-05-01 14:19:48 -0600

    storyboards added

---
+++ Journal/EntryController.swift
@@ -0,0 +1,9 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Forrest Syrett on 5/1/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/EntryDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Forrest Syrett on 5/1/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController, UITextFieldDelegate {
+    
+  
+    
+    
+    
+    
+    
+}
